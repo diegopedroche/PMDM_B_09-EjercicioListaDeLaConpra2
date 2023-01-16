0dd6632628b0981c798f0377797b3e74749adbea
eduard <eduard@cieep.com>
2023-01-16 12:34:28 +0100

    Initial commit

---
+++ app/src/main/java/com/cieep/pmdm_b_09_ejerciciolistadelaconpra/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.cieep.pmdm_b_09_ejerciciolistadelaconpra;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
